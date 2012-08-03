492e2004da7c3eb9c36246ffa498f38a47ba2069
owainlewis <owain@owainlewis.com>
2012-08-03 13:47:43 +0100

    refactor and new methods for types

--- src/clojure_mail/core.clj
+++ src/clojure_mail/core.clj
@@ -5,12 +5,11 @@
            [javax.mail.search FlagTerm]))
 
 ;; Focus will be more on the reading and parsing of emails.
-
+;; Very rough first draft ideas not suitable for production
 ;; Sending email is more easily handled by other libs
-
 ;; IMAP client interface
 
-(defn IMAP [] )
+(defprotocol IMAP [] )
 
 (def gmail {:protocol "imaps" :server "imap.gmail.com"})
 
@@ -75,14 +74,29 @@
   [^javax.mail.internet.MimeMultipart msg]
   (.getContentType msg))
 
+;; There are a ton of these methods that need adding
+
 (defn get-msg-size
   "Returns message size in bytes"
   [msg]
   (.getSize msg))
 
+(defn is-mime-type?
+  [msg type]
+  (.isMimeType msg type))
+  
+(defn get-body-text
+  "Determine the function to call to get the body text of a message"
+  [msg type]
+  (condp = type
+    "multipart/alternative" :multipart
+    "text/html" :html
+    "text/plain" :plain
+    (str "unexpected type, \"" type \")))
+
 (defn get-msg-parts
   [^javax.mail.internet.MimeMultipart msg]
-  (let [no-parts (get (bean msg) :count)
+  (let [no-parts (get (clojure.core/bean msg) :count)
         parts (map #(.getBodyPart msg %) (range no-parts))]
     parts))
 
@@ -98,7 +112,7 @@
   [message]
   (doseq [[uid msg] message]
     (println 
-      (format "%s - %s" uid (bean msg)))))
+      (format "%s - %s" uid (clojure.core/bean msg)))))
   
 (defn dump [msgs]
   (doseq [[uid msg] msgs]
