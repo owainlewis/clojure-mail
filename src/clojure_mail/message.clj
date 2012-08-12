2748066799b2075992d461eae0f8c95cbd2d9d95
owainlewis <owain@owainlewis.com>
2012-08-12 12:31:53 +0100

    mostly working now. Needs tests and major refactoring

--- src/clojure_mail/message.clj
+++ src/clojure_mail/message.clj
@@ -1,4 +1,5 @@
-(ns clojure-mail.message)
+(ns clojure-mail.message
+  (import [javax.mail.internet MimeMultipart]))
 
 (defn from [m]
   (.getFrom m))
@@ -22,9 +23,34 @@
 (defn encoding [m]
   (.getEncoding m))
 
+(defn get-content [m]
+  (.getContent m))
+
 (defn message-headers
   "Returns all the headers from a message"
   [^com.sun.mail.imap.IMAPMessage msg]
   (let [headers (.getAllHeaders msg)
         results (enumeration-seq headers)]
     (map #(vector (.getName %) (.getValue %)) results)))
+
+(defn multipart? [m]
+  ^{:doc "Returns true if a message is a multipart email"}
+  (.startsWith (content-type m) "multipart"))
+
+(defn read-multi [mime-multi-part]
+  (let [count (.getCount mime-multi-part)]
+    (for [part (map #(.getBodyPart mime-multi-part %) (range count))]
+      (if (multipart? part)
+        (.getContent part)
+        part))))
+
+(defn message-parts
+  [^javax.mail.internet.MimeMultipart msg]
+  (when (multipart? msg)
+    (read-multi (get-content msg))))
+
+(defn message-body-map
+  "Read all the body content from a message"
+  [msg]
+  (let [parts (message-parts msg)]
+    (map #(.getContent %) parts)))
