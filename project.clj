f9f76285cc38faf93d6d351bbeb129d6ab547f86
owain lewis <owain@owainlewis.com>
2014-03-11 12:35:14 +0000

    bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojure-mail "0.1.4"
+(defproject clojure-mail "0.1.5"
   :description "Clojure Email Library"
   :url "https://github.com/forward/clojure-mail"
   :license {:name "Eclipse Public License"
