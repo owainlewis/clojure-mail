79ed0bdfb85b83c6ff687f233775f869025c86bd
Owain Lewis <owain@owainlewis.com>
2019-06-25 20:36:05 +0100

    Release 1.8 and update deploy config

--- project.clj
+++ project.clj
@@ -7,5 +7,7 @@
                  [org.jsoup/jsoup "1.8.3"] ;; for cleaning up messy html messages
                  [com.sun.mail/javax.mail "1.5.5"]
                  [medley "1.0.0"]]
+  :deploy-repositories [["releases" {:url "https://clojars.org/repo"
+                                     :creds :gpg}]]
   :plugins [[lein-cljfmt "0.3.0"]]
   :profiles {:dev {:dependencies [[com.icegreen/greenmail "1.4.1"]]}})
